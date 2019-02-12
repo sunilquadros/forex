f7104e99d4760917aa8876cc8f5eb11e6177fba6
root <root@localhost.localdomain>
2019-02-12 07:26:58 -0600

    3rd Update to forex

---
+++ src/main/java/com/example/demo/SpringBootMicroserviceForexService1Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceForexService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceForexService1Application.class, args);
+	}
+}
